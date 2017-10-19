19628ad83ac076de485d64a5d9f7b1f4e7b7989e
unique <podamibe@Uniques-Macbook-pro.local>
2017-10-19 11:22:16 +0545

    added signup and forms

--- routes.py
+++ routes.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import forms
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
+
+app.secret_key = 'testingforsimething'
 
 
 @app.route("/")
@@ -8,9 +11,18 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/about")
+@app.route("/abouts")
 def about():
     return render_template("about.html")
+
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    form = forms.SignupForm()
+    if request.method == 'POST':
+        return "sucess"
+    elif request.method == 'GET':
+        return render_template("signup.html", form=form)
 
 
 if __name__ == "__main__":
