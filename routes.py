66daa7ece961638480bb15f33ce06d218617c4c7
unique <podamibe@Uniques-Macbook-pro.local>
2017-10-19 15:39:48 +0545

    added support for database

--- routes.py
+++ routes.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request
 import forms
+from models import db, User
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:0@localhost:5432/learningflask'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'testingforsimething'
+db.init_app(app)
 
 
 @app.route("/")
@@ -20,7 +23,14 @@ def about():
 def signup():
     form = forms.SignupForm()
     if request.method == 'POST':
-        return "sucess"
+        if form.validate() == False:
+            return render_template("signup.html", form=form)
+        else:
+            newuser = User(form.first_name.data, form.last_name.data,
+                           form.email.data, form.password.data)
+            db.session.add(newuser)
+            db.session.commit()
+            return "sucess"
     elif request.method == 'GET':
         return render_template("signup.html", form=form)
 
